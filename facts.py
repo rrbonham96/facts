e77b261e8af6231a5629c17c96a77d5f0f8b585c
Reilly Bonham <28878714+rrbonham96@users.noreply.github.com>
2018-08-03 19:14:23 -0500

    Switch to utf-8 encoding for open()

--- facts.py
+++ facts.py
@@ -2,7 +2,7 @@ import pyperclip, random
 from time import sleep
 
 if __name__ == '__main__':
-    with open("facts.txt") as facts:
+    with open("facts.txt", encoding='utf-8') as facts:
         fact_list = facts.read().split('\n')
 
         while True:
