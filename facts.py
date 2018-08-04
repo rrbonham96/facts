1ef80530925b7b3b503c798bf10ec63c0fd45caa
Reilly Bonham <28878714+rrbonham96@users.noreply.github.com>
2018-08-03 19:04:36 -0500

    Initial Commit

---
+++ facts.py
@@ -0,0 +1,11 @@
+import pyperclip, random
+from time import sleep
+
+if __name__ == '__main__':
+    with open("facts.txt") as facts:
+        fact_list = facts.read().split('\n')
+
+        while True:
+            i = random.randrange(0, len(fact_list) - 1)
+            pyperclip.copy(fact_list[i])
+            sleep(3)
